b83560d181bdda3c9dd12e9df9443dc8a1d4f1a4
ducc <spielerc@gmail.com>
2023-07-26 16:36:31 -0600

    Update RandomOutfit, look at nearby players

---
+++ code/Terry.Animation.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox;
+
+namespace TerryNpc;
+
+public partial class Terry
+{
+    public Vector3 LookAtPoint { get; set; }
+    public void Animate()
+    {
+        new CitizenAnimationHelper(this)
+            .WithLookAt(LookAtPoint);
+    }
+}
--- code/Terry.cs
+++ code/Terry.cs
@@ -1,10 +1,6 @@
 ﻿using Sandbox;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TerryNpc;
 
 namespace TerryNpc;
 
@@ -28,7 +24,23 @@ public partial class Terry : AnimatedEntity
 		Outfit.DressEntity(this);
 	}
 
-	private DamageInfo lastDamage;
+	[GameEvent.Tick.Server]
+	protected void OnServerTick()
+	{
+		Animate();
+
+		var nearestPlayer = FindNearestPerson(100f, true);
+		if (nearestPlayer.IsValid())
+		{
+			LookAtPoint = nearestPlayer.Position;
+		}
+		else
+		{
+			LookRandomly();
+		}
+	}
+
+    private DamageInfo lastDamage;
 
 	public override void TakeDamage(DamageInfo info)
 	{
@@ -70,4 +82,35 @@ public partial class Terry : AnimatedEntity
 
 		Delete();
 	}
+
+    protected Entity FindNearestPerson(float radius, bool withEyeContact)
+    {
+        var people = Entity
+			.FindInSphere(Position, radius)
+			.Where(e => e.Tags.Has("player") || e.Tags.Has("npc"));
+		IEnumerable<Entity> nearbyPeople = people
+			.Where(p => p.Position.Distance(Position) <= radius)
+			.OrderBy(p => p.Position.Distance(Position));
+		if (withEyeContact)
+		{
+			nearbyPeople = nearbyPeople
+				.Where(p => p.AimRay.Forward.Dot(AimRay.Forward) < 0f);
+        }
+		return nearbyPeople.FirstOrDefault();
+    }
+
+    private TimeUntil StopLookTime { get; set; }
+
+    private void LookRandomly()
+    {
+        if (!StopLookTime)
+        {
+            return;
+        }
+
+        StopLookTime = Game.Random.Float(0.25f, 15f);
+        var frontPos = Position + Rotation.Forward * 400f + Vector3.Zero.WithZ(64f);
+        var frontBox = BBox.FromPositionAndSize(frontPos, 180f);
+        LookAtPoint = frontBox.RandomPointInside;
+    }
 }
